86661eecea013bc51048ef7dd2587f549a69924a
MINH <nguyentriminh505@gmail.com>
2023-07-26 16:48:53 +0700

    fix middleware

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ use App\Http\Controllers\TestMailController;
 use App\Http\Controllers\HistoryController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\CheckLogin;
+use App\Http\Middleware\CheckTypeUser;
+use App\Http\Middleware\CheckTypeUser2;
 use App\Mail\SendAdmin;
 use Illuminate\Support\Facades\Mail;
 
@@ -40,31 +42,36 @@ Route::post('/login-process', [AuthenticateController::class, 'loginProcess'])->
 Route::post('/login-user-process', [AuthenticateController::class, 'loginUserProcess'])->name('login-user-process');
 
 Route::middleware([CheckLogin::class])->group(function(){
-    Route::get('/logout',[AuthenticateController::class,'logout'])->name('logout');
-    Route::get('/dashboard',[StatisticalController::class,'index'])->name('dashboard');
-    Route::resource('profile',ProfileController::class);
-    Route::resource('course',CourseController::class);
-    Route::resource('class',GradeController::class);
-    Route::resource('student',StudentController::class);
-    Route::resource('bill',BillController::class);
-    Route::resource('billdetail',BillDetailController::class);
-    Route::resource('subject',SubjectController::class);
-    Route::resource('major',MajorController::class);
-    Route::resource('book',BookController::class);
-    Route::resource('listbook',ListBookController::class);
-    Route::get('/AddCart/{id}',[CartController::class,'AddCart']);
-    Route::get('/DeleteItemCart/{id}',[CartController::class,'DeleteItemCart']);
-    Route::get('/UpdateItemCart/{id}/{qty}',[CartController::class,'UpdateItemCart']);
-    Route::get('/Order',[CartController::class,'Order']);
-    Route::get('/CancelOrder/{id}',[CartController::class,'CancelOrder']);
-    Route::post('/EditAll',[CartController::class,'EditAll']);
-    Route::get('/DeleteAll',[CartController::class,'DeleteAll']);
-    Route::get('/testmail',[TestMailController::class,'testmail'])->name('testmail');
-    // Route::get('/mycart', [CartController::class,'index']);
-    Route::get('/mycart', function () {
-        return view('cart');
+    Route::middleware([CheckTypeUser::class])->group(function(){
+        Route::get('/dashboard',[StatisticalController::class,'index'])->name('dashboard');
+        Route::resource('course',CourseController::class);
+        Route::resource('class',GradeController::class);
+        Route::resource('student',StudentController::class);
+        Route::resource('bill',BillController::class);
+        Route::resource('billdetail',BillDetailController::class);
+        Route::resource('subject',SubjectController::class);
+        Route::resource('major',MajorController::class);
+        Route::resource('book',BookController::class);
     });
-    Route::post('/epayment',[CartController::class,'epayment']);
-    Route::get('/payreturn',[CartController::class,'payreturn']);
-    Route::resource('history',HistoryController::class);
+    
+    Route::middleware([CheckTypeUser2::class])->group(function(){
+        Route::resource('listbook',ListBookController::class);
+        Route::get('/AddCart/{id}',[CartController::class,'AddCart']);
+        Route::get('/DeleteItemCart/{id}',[CartController::class,'DeleteItemCart']);
+        Route::get('/UpdateItemCart/{id}/{qty}',[CartController::class,'UpdateItemCart']);
+        Route::get('/Order',[CartController::class,'Order']);
+        Route::get('/CancelOrder/{id}',[CartController::class,'CancelOrder']);
+        Route::post('/EditAll',[CartController::class,'EditAll']);
+        Route::get('/DeleteAll',[CartController::class,'DeleteAll']);
+        Route::post('/epayment',[CartController::class,'epayment']);
+        Route::get('/payreturn',[CartController::class,'payreturn']);
+        Route::resource('history',HistoryController::class);
+        Route::get('/mycart', function () {
+            return view('cart');
+        });
+    });
+
+    Route::get('/logout',[AuthenticateController::class,'logout'])->name('logout');
+    Route::resource('profile',ProfileController::class);
+    Route::get('/testmail',[TestMailController::class,'testmail'])->name('testmail');
 });
